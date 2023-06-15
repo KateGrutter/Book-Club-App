447ac12c488de23455825f22a39d65e105c51b94
Bailey Cytacki-Lewis <baileycytackilewis@gmail.com>
2023-06-14 21:29:29 -0400

    Login form

--- bookclubapp/frontend/src/components/BookClubHome.tsx
+++ bookclubapp/frontend/src/components/BookClubHome.tsx
@@ -1,10 +1,12 @@
 import { CurrentBook } from "./CurrentBook";
 import { DiscussionBoard } from "./DiscussionBoard/DiscussionBoard";
+import { Login } from "./LoginForm";
 
 export function BookClubHome() {
 
     return(
         <div>
+            <Login></Login>
             <CurrentBook />
             <DiscussionBoard></DiscussionBoard>
         </div>
